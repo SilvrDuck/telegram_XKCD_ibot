fe0b1ac9edb8f58ffe9ecb030e8cb4ef4ac5d7bf
Thibault Viglino <thibault.viglino@visium.ch>
2026-01-11 20:55:22 +0100

    move escaping out of parser

--- src/main/scala/me/ex0ns/comicbot/models/Comic.scala
+++ src/main/scala/me/ex0ns/comicbot/models/Comic.scala
@@ -3,9 +3,7 @@ package me.ex0ns.comicbot.models
 import com.bot4s.telegram.api.RequestHandler
 import com.bot4s.telegram.methods.{ParseMode, SendMessage, SendPhoto}
 import com.bot4s.telegram.models.InputFile
-import com.typesafe.scalalogging.Logger
 import me.ex0ns.comicbot.helpers.StringHelpers._
-import org.slf4j.LoggerFactory
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -21,32 +19,22 @@ final case class Comic(
     views: Option[Int]
 ) extends Notification {
 
-  private val logger = Logger(LoggerFactory.getLogger(classOf[Comic]))
-
   override def notify(group: Group, noteTemplate: Option[String])(implicit
       request: RequestHandler[Future]
   ): Future[Unit] = {
-    // alt is MdV2: already MarkdownV2-ready (escaped at creation time)
-    // Other fields are plain text and need escaping
-    logger.info(s"[DEBUG] Comic #$num - raw title: $title")
-    logger.info(s"[DEBUG] Comic #$num - title.bold: ${title.bold}")
-    logger.info(s"[DEBUG] Comic #$num - alt.map(_.value): ${alt.map(_.value)}")
-    logger.info(s"[DEBUG] Comic #$num - link: $link")
-
     val formattedNote = noteTemplate match {
       case Some(template) =>
-        template
-          .replace("{title}", title.escaped)
-          .replace("{alt}", alt.map(_.value).getOrElse(""))
-          .replace("{link}", link.getOrElse("").escaped)
-          .replace("{num}", num.toString)
-          .replace("{img}", img.escaped)
-          .replace("{transcript}", transcript.getOrElse("").escaped)
+        // Escape entire template, then replace escaped placeholders
+        template.escaped
+          .replace("\\{title\\}", title.escaped)
+          .replace("\\{alt\\}", alt.map(_.value).getOrElse(""))
+          .replace("\\{link\\}", link.getOrElse("").escaped)
+          .replace("\\{num\\}", num.toString)
+          .replace("\\{img\\}", img.escaped)
+          .replace("\\{transcript\\}", transcript.getOrElse("").escaped)
       case None =>
         alt.map(_.value).getOrElse("") + link.map(l => s"\n\n${l.escaped}").getOrElse("")
     }
-
-    logger.info(s"[DEBUG] Comic #$num - formattedNote: $formattedNote")
 
     for {
       response <- Future { scalaj.http.Http(img).asBytes }
